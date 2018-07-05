b627f74584cf14499ebead2316a578f6b5e704bc
Jonas Hübotter <me@jonhue.me>
2018-07-05 11:38:52 +0200

    [#LIEF-9777] Allow template urls containing params

--- lib/twilio_call_setup/call.rb
+++ lib/twilio_call_setup/call.rb
@@ -4,10 +4,16 @@ module TwilioCallSetup
   class Call
     def initialize(client, from:, to:, twilio_number:, template_url:)
       client.calls.create(
-        url: "#{template_url}?Contact=#{to}",
+        url: url(to, template_url),
         to: from,
         from: twilio_number
       )
+    end
+
+    private
+
+    def url(to, template_url)
+      "#{template_url}#{template_url.include?('?') ? '&' : '?'}Contact=#{to}"
     end
   end
 end
--- spec/twilio_call_setup/call_spec.rb
+++ spec/twilio_call_setup/call_spec.rb
@@ -4,13 +4,14 @@ require "spec_helper"
 require "twilio-call-setup"
 
 describe TwilioCallSetup::Call do
+  let(:template_url) { "https://handler.twilio.com/twiml/ABCDEFG" }
   subject(:twilio_call) do
     described_class.new(
       client_double,
       from: "+49123456789",
       to: "+49987654321",
       twilio_number: "+491324354657",
-      template_url: "https://handler.twilio.com/twiml/ABCDEFG"
+      template_url: template_url
     )
   end
 
@@ -26,6 +27,20 @@ describe TwilioCallSetup::Call do
       )
 
       twilio_call
+    end
+
+    context "template_url contains params" do
+      let(:template_url) { "https://handler.twilio.com/twiml/ABCDEFG?ObjectId=1" }
+
+      it "initiates a call" do
+        expect(calls_double).to receive(:create).with(
+          url: "https://handler.twilio.com/twiml/ABCDEFG?ObjectId=1&Contact=+49987654321",
+          to: "+49123456789",
+          from: "+491324354657"
+        )
+
+        twilio_call
+      end
     end
   end
 end
